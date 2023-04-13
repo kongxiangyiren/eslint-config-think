07e46ecc7dbd2e890d326effc86b670dc758f843
空巷一人 <1530688385@qq.com>
2023-04-13 14:41:08 +0800

    删除babel配置

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    requireConfigFile: false,
     ecmaFeatures: {
       experimentalObjectRestSpread: true
     },
